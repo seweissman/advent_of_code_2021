7e1d02aa18dcd610051115794596388e85c1cfee
Sarah Weissman <seweissman@gmail.com>
2021-12-27 01:00:09 -0500

    day 21 wipdi

--- day21/day21.py
+++ day21/day21.py
@@ -1,4 +1,5 @@
-from collections import defaultdict
+from functools import cache
+
 class GameBoard:
 
     def __init__(self, initial_locs):
@@ -34,138 +35,37 @@ def test_sample():
     result = game.run_game()
     assert result == 739785
 
-    n_paths_to_win1, n_paths_to_win2, win1_states, win2_states, all_states_p1, all_states_p2, end_states_p1, end_states_p2 \
-        = find_paths_to_end(4, 8, 20)
-
-    n_paths_to_win1_ex, n_paths_to_win2_ex, win1_states_ex, win2_states_ex, all_states_p1_ex, all_states_p2_ex, end_states_p1_ex, end_states_p2_ex \
-        = find_paths_to_end_exhaust(4, 8, 20)
-
-    # Round 1
-    # 4,0
-    # 8,0
-    # rolls: 11, 12, 21, 22
-    # rolls: 11, 12, 21, 22
-    # 11 11 6,6  10,10 ---> 2 wins
-    # 11 12 6,6  1, 1
-    # 11 21 6,6  1, 1
-    # 11 22 6,6  2, 2
-    # 12 11 7,7  10,10 ---> 1 wins  ((6, 7), (9, 10), 1), x
-    # 12 12 7,7  1,1 ---> 1 wins  ((6, 7), (0, 1), 1), x
-    # 12 21 7,7  1,1 ---> 1 wins  ((6, 7), (0, 1), 1), x
-    # 12 22 7,7  2,2 ---> 1 wins  ((6, 7), (1, 2), 1)}x
-    # 21 11 7,7  10,10 ---> 1 wins  ((6, 7), (9, 10), 1), x
-    # 21 12 7,7  1,1 ---> 1 wins ((6, 7), (0, 1), 1), x
-    # 21 21 7,7  1,1 ---> 1 wins  ((6, 7), (0, 1), 1), x
-    # 21 22 7,7  2,2 ---> 1 wins  ((6, 7), (1, 2), 1)}x
-    # 22 11 8,8  10,10 ---> 1 wins ((7, 8), (9, 10), 1), x
-    # 22 12 8,8  1,1 ---> 1 wins ((7, 8), (0, 1), 1), x
-    # 22 21 8,8  1,1 ---> 1 wins  ((7, 8), (0, 1), 1), x
-    # 22 22 8,8  2,2 ---> 1 wins  ((7, 8), (1, 2), 1), x
+    paths1, paths2 = find_paths_to_end((4, 0), (8, 0), 21)
+    assert paths1 == 444356092776315
+    assert paths2 == 341960390180808
 
 
+@cache
+def find_paths_to_end(state1, state2, winning_score):
+    loc1, score1 = state1
+    loc2, score2 = state2
 
-
-# 3 x 16 wins for 1 in round 2 = 48 wins for 1, + 12 wins in round 1 = 60
-# 1 win for 2
-
-# Round 2
-    # 6,6  1, 1
-    # rolls: 11, 12, 21, 22
-    # rolls: 11, 12, 21, 22
-    # 11 11 8,14  3,4 ---> 1 wins  ((7, 14), (2, 4), 2), 
-    # 11 12 8,14  4,5 ---> 1 wins  ((7, 14), (3, 5), 2), 
-    # 11 21 8,14  4,5 ---> 1 wins  ((7, 14), (3, 5), 2), 
-    # 11 22 8,14  5,6 ---> 1 wins ((7, 14), (4, 6), 2), 
-    # 12 11 9,15  3,4 ---> 1 wins ((8, 15), (2, 4), 2), 
-    # 12 12 9,15  4,5 ---> 1 wins ((8, 15), (3, 5), 2), 
-    # 12 21 9,15  4,5 ---> 1 wins ((8, 15), (3, 5), 2), 
-    # 12 22 9,15  5,6 ---> 1 wins  ((8, 15), (4, 6), 2),
-    # 21 11 9,15  3,4 ---> 1 wins  ((8, 15), (2, 4), 2), 
-    # 21 12 9,15  4,5 ---> 1 wins  ((8, 15), (3, 5), 2), 
-    # 21 21 9,15  4,5 ---> 1 wins  ((8, 15), (3, 5), 2), 
-    # 21 22 9,15  5,6 ---> 1 wins  ((8, 15), (4, 6), 2), 
-    # 22 11 10,16  3,4 ---> 1 wins   {((9, 16), (2, 4), 2), 
-    # 22 12 10,16  4,5 ---> 1 wins   ((9, 16), (3, 5), 2), 
-    # 22 21 10,16  4,5 ---> 1 wins   ((9, 16), (3, 5), 2), 
-    # 22 22 10,16  5,6 ---> 1 wins   ((9, 16), (4, 6), 2), 
-
-    # 6,6  2, 2
-    # rolls: 11, 12, 21, 22
-    # rolls: 11, 12, 21, 22
-    # 11 11 8,14  4,6 ---> 1 wins  ((7, 14), (3, 6), 2),  x
-    # 11 12 8,14  5,7 ---> 1 wins ((7, 14), (4, 7), 2),  x
-    # 11 21 8,14  5,7 ---> 1 wins ((7, 14), (4, 7), 2), x
-    # 11 22 8,14  6,8 ---> 1 wins ((7, 14), (5, 8), 2),  x
-    # 12 11 9,15  4,6 ---> 1 wins ((8, 15), (3, 6), 2), x
-    # 12 12 9,15  5,7 ---> 1 wins ((8, 15), (4, 7), 2),  x
-    # 12 21 9,15  5,7 ---> 1 wins ((8, 15), (4, 7), 2), x
-    # 12 22 9,15  6,8 ---> 1 wins  ((8, 15), (5, 8), 2), x
-    # 21 11 9,15  4,6 ---> 1 wins  ((8, 15), (3, 6), 2), x
-    # 21 12 9,15  5,7 ---> 1 wins  ((8, 15), (4, 7), 2), x 
-    # 21 21 9,15  5,7 ---> 1 wins  ((8, 15), (4, 7), 2),  x
-    # 21 22 9,15  6,8 ---> 1 wins  ((8, 15), (5, 8), 2), x
-    # 22 11 10,16  4,6 ---> 1 wins   ((9, 16), (3, 6), 2), x
-    # 22 12 10,16  5,7 ---> 1 wins   ((9, 16), (4, 7), 2), x
-    # 22 21 10,16  5,7 ---> 1 wins   ((9, 16), (4, 7), 2),  x
-    # 22 22 10,16  6,8 ---> 1 wins  ((9, 16), (5, 8), 2),  x
-
-    print("Paths:", n_paths_to_win1_ex, n_paths_to_win1)
-    assert n_paths_to_win1 == n_paths_to_win1_ex
-    assert n_paths_to_win2 == n_paths_to_win2_ex
-    assert win1_states == win1_states_ex
-    assert win2_states == win2_states_ex
-    
-def find_paths_to_end_exhaust(start1, start2, winning_score):
-    stack = [((start1 - 1, 0), (start2 - 1, 0), 0)]
-    paths_to_win1 = 0
-    win1_states = set()
-    paths_to_win2 = 0
-    win2_states = set()
-    all_states1 = set()
-    all_states2 = set()
-    end_states1 = set()
-    end_states2 = set()
-    while stack:
-        state1, state2, round = stack.pop()
-        all_states1.add((state1[0], state1[1], round))
-        all_states2.add((state2[0], state2[1], round))
-        loc1, score1 = state1
-        loc2, score2, = state2
-        if score1 >= winning_score:
-            end_states1.add((state1[0], state1[1], round))
-            end_states2.add((state2[0], state2[1], round))
-            paths_to_win1 += 1
-            win1_states.add((state1, state2, round))
-            continue
-        else:
-            if score2 >= winning_score:
-                paths_to_win2 += 1
-                end_states1.add((state1[0], state1[1], round))
-                end_states2.add((state2[0], state2[1], round))
-                win2_states.add((state1, state2, round))
+    wins1 = 0
+    wins2 = 0
+    for roll1 in TWO_ROLLS:
+        for roll2 in TWO_ROLLS:
+            new_loc1 = (loc1 + roll1 - 1) % 10 + 1
+            new_score1 = score1 + new_loc1
+            if new_score1 >= winning_score:
+                wins1 += 1
+                break
+            new_loc2 = (loc2 + roll2 - 1) % 10 + 1
+            new_score2 = score2 + new_loc2
+            if new_score2 >= winning_score:
+                wins2 += 1
                 continue
-        for r1_1 in (1,2):
-            for r1_2 in (1,2):
-                roll1 = r1_1 + r1_2
-                new_loc1 = (loc1 + roll1) % 10
-                new_score1 = score1 + new_loc1 + 1
-                for r2_1 in (1,2):
-                    for r2_2 in (1,2):
-                        roll2 = r2_1 + r2_2
-                        new_loc2 = (loc2 + roll2) % 10
-                        new_score2 = score2 + new_loc2 + 1
-                        stack.append(((new_loc1, new_score1), (new_loc2, new_score2), round+1))
-    print("Paths to win: ", paths_to_win1, paths_to_win2)
-    print("N winning states", len(win1_states), len(win2_states))
-    print("All states", len(all_states1), len(all_states2))
-    print([(state1, state2, round) for (state1, state2, round) in win1_states if round == 1])
-    #print(win1_states)
-    return (paths_to_win1, paths_to_win2, win1_states, win2_states, 
-            all_states1, all_states2, end_states1, end_states2)
+            sub_wins1, sub_wins2 = find_paths_to_end((new_loc1, new_score1), (new_loc2, new_score2), winning_score)
+            wins1 += sub_wins1
+            wins2 += sub_wins2
+    return wins1, wins2
 
-
-THREE_ROLLS =  [(3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1)]
-TWO_ROLLS =  [(2, 1), (3, 2), (4, 1)]
+THREE_ROLLS =  [3, 4, 4, 4, 5, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7, 7, 8, 8, 8, 9]
+TWO_ROLLS = [2, 3, 3, 4]
 
 def find_paths_to_end(start1, start2, winning_score):
     all_states_p1 = find_all_states(start1, winning_score)
@@ -199,8 +99,6 @@ def find_paths_to_end(start1, start2, winning_score):
 
     seen = set()
     while states:
-        print("\n\nIteration:")
-        print("Paths to end: ", paths_to_end1[((3, 0), (7, 0), 0)])
         new_states = set()
         for state in states:
             p1_state, p2_state, round = state
@@ -218,113 +116,31 @@ def find_paths_to_end(start1, start2, winning_score):
                     prev_score2 = score2 - (loc2 + 1)
                     new_state = ((prev_loc1, prev_score1),(prev_loc2, prev_score2), prev_round)
                     new_states.add(new_state)
-        #print("New states:", new_states)
-        for new_state1, new_state2, new_round in sorted(new_states, key=lambda s: -1*s[2]):
-            if (new_state1, new_state2, new_round) in seen:
-                continue
-            new_loc1, new_score1 = new_state1
-            new_loc2, new_score2 = new_state2
-            for roll1, mult1 in TWO_ROLLS:
-                loc1 = (new_loc1 + roll1) % 10
-                score1 = new_score1 + (loc1 + 1)
-                for roll2, mult2 in TWO_ROLLS:
-                    loc2 = (new_loc2 + roll2) % 10
-                    score2 = new_score2 + (loc2 + 1)
-                    if (
-                        (((new_loc1, new_score1), new_round) == ((3, 0), 0) and ((new_loc2, new_score2), new_round) == ((7, 0), 0)) or
-                        (((new_loc1, new_score1), new_round) == ((5, 6), 1) and ((new_loc2, new_score2), new_round) == ((1, 2), 1)) 
-                        ):
-                        amount_to_add = mult1*mult2*paths_to_end1[((loc1, score1), (loc2, score2), new_round + 1)]
-                        #print("loc1", "loc2", new_loc1, new_loc2, loc1, score1, loc2, score2, roll1, roll2, "Adding: ", amount_to_add)
-                        #print("Adding:", amount_to_add)
-
-                    paths_to_end1[((new_loc1, new_score1), (new_loc2, new_score2), new_round)] += mult1*mult2*paths_to_end1[((loc1, score1), (loc2, score2), new_round + 1)]
-
-        states = new_states
-        seen = seen.union(new_states)
-
-    paths_to_end2 = defaultdict(int)
-    states = set()
-    for state in win2_states:
-        states.add(state)
-        paths_to_end2[state] = 1
-
-    seen = set()
-    while states:
-        print("\n\nIteration:")
-        print("Paths to end: ", paths_to_end2[((3, 0), (7, 0), 0)])
-        new_states = set()
-        for state in states:
-            p1_state, p2_state, round = state
-            loc1, score1 = p1_state
-            loc2, score2 = p2_state
-            if round == 0 or ((loc1, score1, round) not in all_states_p1) or ((loc2, score2, round) not in all_states_p2):
-                continue
-            #print(state)
-            prev_round = round - 1
-            for roll1, mult1 in TWO_ROLLS:
-                prev_loc1 = (loc1 - roll1) % 10
-                prev_score1 = score1 - (loc1 + 1)
-                for roll2, mult2 in TWO_ROLLS:
-                    prev_loc2 = (loc2 - roll2) % 10
-                    prev_score2 = score2 - (loc2 + 1)
-                    new_state = ((prev_loc1, prev_score1),(prev_loc2, prev_score2), prev_round)
-                    new_states.add(new_state)
-        #print("New states:", new_states)
-        for new_state1, new_state2, new_round in sorted(new_states, key=lambda s: -1*s[2]):
-            if (new_state1, new_state2, new_round) in seen:
-                continue
-            new_loc1, new_score1 = new_state1
-            new_loc2, new_score2 = new_state2
-            for roll1, mult1 in TWO_ROLLS:
-                loc1 = (new_loc1 + roll1) % 10
-                score1 = new_score1 + (loc1 + 1)
-                for roll2, mult2 in TWO_ROLLS:
-                    loc2 = (new_loc2 + roll2) % 10
-                    score2 = new_score2 + (loc2 + 1)
-                    if (
-                        (((new_loc1, new_score1), new_round) == ((3, 0), 0) and ((new_loc2, new_score2), new_round) == ((7, 0), 0)) or
-                        (((new_loc1, new_score1), new_round) == ((5, 6), 1) and ((new_loc2, new_score2), new_round) == ((1, 2), 1)) 
-                        ):
-                        amount_to_add = mult1*mult2*paths_to_end1[((loc1, score1), (loc2, score2), new_round + 1)]
-                        #print("loc1", "loc2", new_loc1, new_loc2, loc1, score1, loc2, score2, roll1, roll2, "Adding: ", amount_to_add)
-                        #print("Adding:", amount_to_add)
-
-                    paths_to_end2[((new_loc1, new_score1), (new_loc2, new_score2), new_round)] += mult1*mult2*paths_to_end2[((loc1, score1), (loc2, score2), new_round + 1)]
-
-        states = new_states
-        seen = seen.union(new_states)
-
-    # for p1_state, p2_state, round in paths_to_end:
-    #     if round == 0:
-    #         print(p1_state, p2_state, paths_to_end[p1_state, p2_state, round])
-    print("Paths to end: ", paths_to_end1[((3, 0), (7, 0), 0)])
-
-    print("Paths:", paths_to_end2[((start1-1, 0), (start2-1, 0), 0)])
-    #print(paths_to_end)
-    return (paths_to_end1[((start1-1, 0), (start2-1, 0), 0)], paths_to_end2[((start1-1, 0), (start2-1, 0), 0)], win1_states, win2_states, 
-            all_states_p1, all_states_p2, end_states_p1, end_states_p2)
-
-#def calc_paths_to_end()
-
-
-def find_all_states(start, winning_score):
-    seen = set()
-    stack = [(start-1, 0, 0)]
-    while stack:
-        loc, score, round = stack.pop()
-        seen.add((loc, score, round))
-        if score >= winning_score:
-            continue
-        for i in range(1,3):
-            for j in range(1, 3):
-                roll = i + j
-                new_loc = (loc + roll) % 10
-                new_score = score + new_loc + 1
-                new_round = round + 1
-                if (new_loc, new_score, new_round) not in seen:
-                    stack.append((new_loc, new_score, new_round))
-    return seen
+-        #print("New states:", new_states)
+-        for new_state1, new_state2, new_round in sorted(new_states, key=lambda s: -1*s[2]):
+-            if (new_state1, new_state2, new_round) in seen:
+-                continue
+-            new_loc1, new_score1 = new_state1
+-            new_loc2, new_score2 = new_state2
+-            for roll1, mult1 in TWO_ROLLS:
+-                loc1 = (new_loc1 + roll1) % 10
+-                score1 = new_score1 + (loc1 + 1)
+-                for roll2, mult2 in TWO_ROLLS:
+-                    loc2 = (new_loc2 + roll2) % 10
+-                    score2 = new_score2 + (loc2 + 1)
+-                    if (
+-                        (((new_loc1, new_score1), new_round) == ((3, 0), 0) and ((new_loc2, new_score2), new_round) == ((7, 0), 0)) or
+-                        (((new_loc1, new_score1), new_round) == ((5, 6), 1) and ((new_loc2, new_score2), new_round) == ((1, 2), 1))
+-                        ):
+-                        amount_to_add = mult1*mult2*paths_to_end1[((loc1, score1), (loc2, score2), new_round + 1)]
+-                        #print("loc1", "loc2", new_loc1, new_loc2, loc1, score1, loc2, score2, roll1, roll2, "Adding: ", amount_to_add)
+-                        #print("Adding:", amount_to_add)
+-
+-                    paths_to_end1[((new_loc1, new_score1), (new_loc2, new_score2), new_round)] += mult1*mult2*paths_to_end1[((loc1, score1), (loc2, score2), new_round + 1)]
+-
+-        states = new_states
+-        seen = seen.union(new_states)
+-
 
 
 if __name__ == "__main__":
@@ -332,3 +148,40 @@ if __name__ == "__main__":
     result = game.run_game()
     print("Part 1: ", result)
 
+    n_paths_to_win1, n_paths_to_win2 = find_paths_to_end((7,0), (8,0), 21)
+    print("Part2: ", n_paths_to_win1, n_paths_to_win2)
+
+    n_paths_to_win1, n_paths_to_win2 = find_paths_to_end((4,0), (8,0), 7)
+    print("Test2: ", n_paths_to_win1, n_paths_to_win2)
+
+
+# -    # Round 1
+# -    # 4,0
+# -    # 8,0
+# -    # rolls: 11, 12, 21, 22
+# -    # rolls: 11, 12, 21, 22
+# -    # 11 11 6,6  10,10 ---> 2 wins
+# -    # 11 12 6,6  1, 1
+# -    # 11 21 6,6  1, 1
+# -    # 11 22 6,6  2, 2
+# -    # 12 11 7,7  10,10 ---> 1 wins  ((6, 7), (9, 10), 1), x
+# -    # 21 11 7,7  10,10 ---> 1 wins  ((6, 7), (9, 10), 1), x
+# -    # 22 11 8,8  10,10 ---> 1 wins ((7, 8), (9, 10), 1), x
+
+# -
+# -# Round 2
+# -    # 6,6  1, 1
+# -    # rolls: 11, 12, 21, 22
+# -    # rolls: 11, 12, 21, 22
+# -    # 11 11 8,14  3,4 ---> 1 wins  ((7, 14), (2, 4), 2),
+# -    # 12 11 9,15  3,4 ---> 1 wins ((8, 15), (2, 4), 2),
+# -    # 21 11 9,15  3,4 ---> 1 wins  ((8, 15), (2, 4), 2),
+# -    # 22 11 10,16  3,4 ---> 1 wins   {((9, 16), (2, 4), 2),
+# -
+# -    # 6,6  2, 2
+# -    # rolls: 11, 12, 21, 22
+# -    # rolls: 11, 12, 21, 22
+# -    # 11 11 8,14  4,6 ---> 1 wins  ((7, 14), (3, 6), 2),  x
+# -    # 12 11 9,15  4,6 ---> 1 wins ((8, 15), (3, 6), 2), x
+# -    # 21 11 9,15  4,6 ---> 1 wins  ((8, 15), (3, 6), 2), x
+# -    # 22 11 10,16  4,6 ---> 1 wins   ((9, 16), (3, 6), 2), x
