5d55d32a7d2bf4f3ceb083bda65b220b7a485237
Sarah Weissman <seweissman@gmail.com>
2021-12-30 12:03:14 -0500

    code cleanup

--- day23/day23.py
+++ day23/day23.py
@@ -1,15 +1,17 @@
+from heapq import heappop, heappush
 
-AMPHIPOD_ENERGY = {
-    "A": 1,
-    "B": 10,
-    "C": 100,
-    "D": 1000
-}
-
-burrow_hallway_positions = [2,4,6,8]
 
 
 class BurrowState:
+    hallway_positions = [2,4,6,8]
+    amphipod_energy = {
+        "A": 1,
+        "B": 10,
+        "C": 100,
+        "D": 1000
+    }
+    amphipod_order = ["A","B","C","D"]
+
     def __init__(self, hallway, burrows):
         self.hallway = list(hallway)
         self.burrows = []
@@ -20,18 +22,17 @@ class BurrowState:
         amphipod, depth = self.get_next_amphipod_in_burrow(burrow_number)
         self.hallway[hallway_space] = amphipod
         self.burrows[burrow_number][depth] = "."
-        move_ct = abs(hallway_space - burrow_hallway_positions[burrow_number]) + 1 + depth
-        move_energy = move_ct * AMPHIPOD_ENERGY[amphipod]
+        move_ct = abs(hallway_space - BurrowState.hallway_positions[burrow_number]) + 1 + depth
+        move_energy = move_ct * BurrowState.amphipod_energy[amphipod]
         return move_energy
 
     def move_from_hallway_to_burrow(self, burrow_number, hallway_space):
-        burrow_hallway_positions = [2,4,6,8]
         depth = self.get_next_empty_spot_in_burrow(burrow_number)
         amphipod = self.hallway[hallway_space]
         self.burrows[burrow_number][depth] = amphipod
         self.hallway[hallway_space] = "."
-        move_ct = abs(hallway_space - burrow_hallway_positions[burrow_number]) + 1 + depth
-        move_energy = move_ct * AMPHIPOD_ENERGY[amphipod]
+        move_ct = abs(hallway_space - BurrowState.hallway_positions[burrow_number]) + 1 + depth
+        move_energy = move_ct * BurrowState.amphipod_energy[amphipod]
         return move_energy
 
     def move_from_burrow_to_burrow(self, burrow_start, burrow_end):
@@ -40,8 +41,8 @@ class BurrowState:
         self.burrows[burrow_end][end_depth] = amphipod
         self.burrows[burrow_start][start_depth] = "."
 
-        move_ct = abs(burrow_hallway_positions[burrow_start] - burrow_hallway_positions[burrow_end]) + 2 + start_depth + end_depth
-        move_energy = move_ct * AMPHIPOD_ENERGY[amphipod]
+        move_ct = abs(BurrowState.hallway_positions[burrow_start] - BurrowState.hallway_positions[burrow_end]) + 2 + start_depth + end_depth
+        move_energy = move_ct * BurrowState.amphipod_energy[amphipod]
         return move_energy
     
     def get_next_amphipod_in_burrow(self, burrow_num):
@@ -63,13 +64,13 @@ class BurrowState:
         return self.burrows[burrow_num][0] == "."
 
     def is_hallway_reachable_from_burrow(self, burrow_num, hallway_space):
-        burrow_hallway_space = burrow_hallway_positions[burrow_num]
+        burrow_hallway_space = BurrowState.hallway_positions[burrow_num]
         min_space = min(hallway_space, burrow_hallway_space)
         max_space = max(hallway_space, burrow_hallway_space)
         return all([self.hallway[sp] == "." for sp in range(min_space, max_space + 1)])
 
     def is_burrow_reachable_from_hallway(self, burrow_num, hallway_space):
-        burrow_hallway_space = burrow_hallway_positions[burrow_num]
+        burrow_hallway_space = BurrowState.hallway_positions[burrow_num]
         min_space = min(hallway_space, burrow_hallway_space)
         max_space = max(hallway_space, burrow_hallway_space)
         if hallway_space == min_space:
@@ -80,15 +81,17 @@ class BurrowState:
 
     def is_amphipod_already_in_home_burrow(self, burrow_num):
         amphipod, depth = self.get_next_amphipod_in_burrow(burrow_num)
-        if all([["A","B","C","D"].index(self.burrows[burrow_num][d]) == burrow_num for d in range(depth, len(self.burrows[burrow_num]))]):
+        if all([BurrowState.amphipod_order.index(self.burrows[burrow_num][d]) == burrow_num 
+                for d in range(depth, len(self.burrows[burrow_num]))]):
             return True
         return False
 
-    def does_burrow_only_have_home_amphipods(self, burrow_num):
+    def has_burrow_only_home_amphipods(self, burrow_num):
         if self.burrows[burrow_num][-1] == ".":
             return True
         amphipod, depth = self.get_next_amphipod_in_burrow(burrow_num)
-        if all([["A","B","C","D"].index(self.burrows[burrow_num][d]) == burrow_num for d in range(depth, len(self.burrows[burrow_num]))]):
+        if all([BurrowState.amphipod_order.index(self.burrows[burrow_num][d]) == burrow_num 
+                for d in range(depth, len(self.burrows[burrow_num]))]):
             return True
         return False
         
@@ -98,6 +101,38 @@ class BurrowState:
                                      tuple(self.burrows[1]),
                                      tuple(self.burrows[2]),
                                      tuple(self.burrows[3]))
+
+    def get_legal_moves(self):
+        legal_moves = []
+        for burrow_num in range(4):
+            if not self.is_empty_burrow(burrow_num):
+                amphipod, depth = self.get_next_amphipod_in_burrow(burrow_num)
+                if self.is_amphipod_already_in_home_burrow(burrow_num):
+                    continue
+                for hallway_space in range(len(self.hallway)):
+                    if hallway_space in BurrowState.hallway_positions:
+                        continue
+                    if self.is_hallway_reachable_from_burrow(burrow_num, hallway_space):
+                        legal_moves.append(("bth", burrow_num, hallway_space))
+                for other_burrow_num in range(4):
+                    if burrow_num != other_burrow_num:
+                        if (BurrowState.amphipod_order.index(amphipod) == other_burrow_num
+                            and self.is_hallway_reachable_from_burrow(burrow_num, 
+                                                            BurrowState.hallway_positions[other_burrow_num])
+                            and self.has_room_burrow(other_burrow_num)
+                            and self.has_burrow_only_home_amphipods(other_burrow_num)):
+                            legal_moves.append(("btb", burrow_num, other_burrow_num))
+        for hallway_space in range(0, 11):
+            amphipod = self.hallway[hallway_space]
+            if amphipod != ".":
+                for burrow_num in range(0,4):
+                    if (self.is_burrow_reachable_from_hallway(burrow_num, hallway_space) 
+                        and self.has_room_burrow(burrow_num)
+                        and BurrowState.amphipod_order.index(amphipod) == burrow_num
+                        and self.has_burrow_only_home_amphipods(burrow_num)):
+                        legal_moves.append(("htb", burrow_num, hallway_space))
+        return legal_moves
+
 
     def make_move(self,move_type, n1, n2):
         if move_type == "bth":
@@ -137,56 +172,23 @@ def test_move_amphipods():
     energy = burrow.move_from_hallway_to_burrow(1, 3)
     assert energy == 30
 
-from heapq import heappop, heappush
 
-def get_legal_moves(burrow):
-    legal_moves = []
-    for burrow_num in range(4):
-        if not burrow.is_empty_burrow(burrow_num):
-            amphipod, depth = burrow.get_next_amphipod_in_burrow(burrow_num)
-            if burrow.is_amphipod_already_in_home_burrow(burrow_num):
-                continue
-            for hallway_space in range(len(burrow.hallway)):
-                if hallway_space in burrow_hallway_positions:
-                    continue
-                if burrow.is_hallway_reachable_from_burrow(burrow_num, hallway_space):
-                    legal_moves.append(("bth", burrow_num, hallway_space))
-            for other_burrow_num in range(4):
-                if burrow_num != other_burrow_num:
-                    if (["A","B","C","D"].index(amphipod) == other_burrow_num
-                        and burrow.is_hallway_reachable_from_burrow(burrow_num, 
-                                                        burrow_hallway_positions[other_burrow_num])
-                        and burrow.has_room_burrow(other_burrow_num)
-                        and burrow.does_burrow_only_have_home_amphipods(other_burrow_num)):
-                        legal_moves.append(("btb", burrow_num, other_burrow_num))
-    for hallway_space in range(0, 11):
-        amphipod = burrow.hallway[hallway_space]
-        if amphipod != ".":
-            for burrow_num in range(0,4):
-                if (burrow.is_burrow_reachable_from_hallway(burrow_num, hallway_space) 
-                    and burrow.has_room_burrow(burrow_num)
-                    and ["A","B","C","D"].index(amphipod) == burrow_num
-                    and burrow.does_burrow_only_have_home_amphipods(burrow_num)):
-                    legal_moves.append(("htb", burrow_num, hallway_space))
-    return legal_moves
 
 def test_legal_moves():
     hallway = [".",".",".","B",".",".",".",".",".",".","."]
     burrows = [["B","A"],[".","D"],["C","C"],["D","A"]]
     burrow = BurrowState(hallway, burrows)
-    legal_moves = get_legal_moves(burrow)
-    print(legal_moves)
+    legal_moves = burrow.get_legal_moves()
+    assert legal_moves == [('bth', 0, 0), ('bth', 0, 1), ('bth', 1, 5), ('bth', 1, 7), ('bth', 1, 9), ('bth', 1, 10), 
+                           ('bth', 3, 5), ('bth', 3, 7), ('bth', 3, 9), ('bth', 3, 10)]
     burrow.make_move("bth",1,5)
-    print(burrow.get_burrow_state())
-    #import pdb; pdb.set_trace()
-    legal_moves = get_legal_moves(burrow)
-    print(legal_moves)
+    legal_moves = burrow.get_legal_moves()
+    assert legal_moves == [('bth', 0, 0), ('bth', 0, 1), ('bth', 3, 7), ('bth', 3, 9), ('bth', 3, 10), ('htb', 1, 3)]
 
 def find_least_energy_solution(start_burrow: BurrowState, final_state):
     paths = []
     heappush(paths, (0, start_burrow.get_burrow_state()))
     seen = set()
-    #import pdb; pdb.set_trace()
     n_paths = 0
     while paths:        
         cost, burrow_state = heappop(paths)
@@ -200,8 +202,7 @@ def find_least_energy_solution(start_burrow: BurrowState, final_state):
             return cost
         seen.add(burrow_state)
         burrow = BurrowState(burrow_state[0], burrow_state[1])
-        legal_moves = get_legal_moves(burrow)
-        #print(burrow_state, legal_moves)
+        legal_moves = burrow.get_legal_moves()
         for move_type, n1, n2 in legal_moves:
             burrow = BurrowState(burrow_state[0], burrow_state[1])
             move_cost = burrow.make_move(move_type, n1, n2)
@@ -246,7 +247,7 @@ def test_sample_part2_state_by_state():
         ("htb",3,10),
     ]
     for move in moves:
-        legal_moves = get_legal_moves(burrow)
+        legal_moves = burrow.get_legal_moves()
         assert move in legal_moves
         move_cost = burrow.make_move(*move)
         cost += move_cost
